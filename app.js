afa122322006d654366f4190bfb44ddc5ef432b1
root <root@cozy.local>
2017-04-20 15:30:15 +0000

    ajout de app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+function updateDocumentList(){
+	cozysdk.defineView('questionnaire', 'all', 'function(doc){ emit(doc); }', function(err, res) {
+		if(err != null) return alert(err);
+		cozysdk.queryView('questionnaire', 'all', {}, function(err, res) {
+			if (err != null) return alert(err);
+			var questionnaires = JSON.parse("" + res);
+			render(questionnaires);
+		});
+	});
+}
+
+function render(questionnaires){
+  var HTML = ''
+  for (var i = 0; i < questionnaires.length; i++) {
+    HTML += '<tr data-id="' + questionnaires[i].id + '">' +
+          +   '<td><label>' + questionnaires[i].key + '</label></td>'
+          + '</tr>';
+  }
+  document.querySelector('.questionnaire-list').innerHTML = HTML;
+}
+
+document.addEventListener("DOMContentLoaded", updateDocumentList);
