0662ae302c63c667b21799aa88bc61bf5dc035e5
root <root@cozy.local>
2017-04-24 15:31:34 +0000

    xcx

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ function updateDocumentList(){
 	byId = function(doc){ if(doc.temps==parseInt(id)) {emit(doc); }}
 	cozysdk.defineView('questionnaire', 'id', byId, function(err, res) {
 		if(err != null) return alert(err);
+		console.log(res)
 		console.log("defineView passé")
 		cozysdk.queryView('questionnaire', 'id', {}, function(err, res) {
 			if (err != null) return alert(err);
+			console.log(res)
 			console.log("queryView passé")
 			render(res);
 		});
