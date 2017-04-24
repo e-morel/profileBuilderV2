86fef249854dad57ce4a3fe967a79c36bc3a9fca
root <root@cozy.local>
2017-04-24 17:03:39 +0000

    xc

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 function updateDocumentList(){
 	console.log("Je passe par updateDocumentList")
 	var id = document.querySelector('.send').value;
-	id = "'"+Number(id)+"'";
+	id = "'"+id+"'";
+	console.log(id=='8151');
 	byId = function(doc){ if(doc.id==id) {emit(doc); }}
 	cozysdk.defineView('questionnaire', 'id', byId, function(err, res) {
 		if(err != null) return alert(err);
