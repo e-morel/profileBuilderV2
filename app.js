b9406ebe374661ec42f83217ecc15d10a288cd32
root <root@cozy.local>
2017-04-25 10:32:38 +0000

    dfdf

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function render(questionnaire){
   console.log(questionnaire);
   for(var donnee in questionnaire){
 	var id = document.querySelector('.send').value;
-	  if(donnee["value"]["id"]==id){
+	  if(donnee["doc"]["id"]==id){
 		cozysdk.find('questionnaire', donnee["id"] , function(err, note){ 
 			if(err != null) return alert(err);
 			console.log(note);
